ece58809e00c45f76cac76e162e0157eb00bb6de
boris <boristondjua@gmail.com>
2024-09-07 15:37:29 +0200

    the sigup view first part is implemented for the login of the user

--- Larissa_Couture/DocShop/accounts/views.py
+++ Larissa_Couture/DocShop/accounts/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import get_user_model
+from django.shortcuts import render, redirectfrom django.contrib.auth import get_user_model, login
 
 # cette fonction get_user_model nous permet de recuprer un object qui correspond a notre model d 'utilisateur
 
@@ -7,9 +6,16 @@ User = get_user_model()
 
 
 def signup(request):
+    if request.method == "POST":
+        # Traiter le formulaire
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = User.objects.create_user(username=username,
+                                        password=password)
+        login(request, user)
+        return redirect("index")
+# la fonction login permet de leir la requette au user creer
     return render(request, 'accounts/signup.html')
 
     # request.POST est un dictionaire donct les clefs username et password defini dans le fichier signu.html
     # La fonction login te permet de connecter L#utlisateur
-
-
