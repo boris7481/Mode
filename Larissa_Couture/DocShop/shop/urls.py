d1e0b5117fdbd93175f3b175f255c0a0f3d6886b
boris <boristondjua@gmail.com>
2024-09-09 20:12:58 +0200

    the view add_to_cart is successful implemented

--- Larissa_Couture/DocShop/shop/urls.py
+++ Larissa_Couture/DocShop/shop/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from store.views import index, product_detail
+from store.views import index, product_detail, add_to_cart
 from accounts.views import signup, logout_user, login_user
 from shop import settings
 
@@ -28,6 +28,7 @@ urlpatterns = [
                   path('login/', login_user, name='login'),
                   path('logout/', logout_user, name='logout'),
                   path('product/<str:slug>', product_detail, name="product"),
+                  path('product/<str:slug>/add-to-cart/', add_to_cart, name="add_to_cart"),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # cette ligne de concatenation permet de gerer tous nos fichiers statique dans un seul dossier Media
--- Larissa_Couture/DocShop/store/views.py
+++ Larissa_Couture/DocShop/store/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
-from store.models import Product
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse
+
+from store.models import Product, Cart, Order
 
 
 def index(request):
@@ -9,6 +11,24 @@ def index(request):
 
 
 def product_detail(request, slug):
-    product = get_object_or_404(Product, slug= slug)
-    return render(request, "store/detail.html", context = {"product" : product})
+    product = get_object_or_404(Product, slug=slug)
+    return render(request, "store/detail.html", context={"product": product})
 
+
+def add_to_cart(request, slug):
+    user = request.user
+    product = get_object_or_404(Product, slug=slug)
+    cart, _ = Cart.objects.get_or_create(user=user)
+    order, created = Order.objects.get_or_create(user = user,
+                                                 product= product)
+
+    if created:
+        cart.orders.add(order)
+        cart.save()
+    else:
+        order.quantity +=1
+        order.save()
+
+    return redirect(reverse("product", kwargs={"slug":slug}))
+
+# tous le code qui de la vue add_to_cart ci dessus te permet de gerer de manier plus efficasse le panier
