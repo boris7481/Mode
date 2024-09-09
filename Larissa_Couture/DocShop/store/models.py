b4431fede93cd870a6f163bc599ad79b1f006267
boris <boristondjua@gmail.com>
2024-09-09 19:09:18 +0200

    creation of the model Order successfully

--- Larissa_Couture/DocShop/store/models.py
+++ Larissa_Couture/DocShop/store/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.urls import reverse
 
+from shop.settings import AUTH_USER_MODEL
+
 
 # Create your models here.
 class Product(models.Model):
@@ -18,3 +20,31 @@ class Product(models.Model):
     # cette methode dois obligatoirement s'appelle comme cela permet de gerer les urls aui niveau de l'administrtion
     def get_absolute_url(self):
         return reverse("product", kwargs={"slug": self.slug})
+
+
+# Articles
+"""
+- Utilisateur
+- Produit
+- Quantite
+- Commande ou nom
+"""
+
+
+class Order(models.Model):
+    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    ordered = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.product.name} {self.quantity}"
+
+
+# Panier
+"""
+- Utilisateur
+- Articles
+- commande ou nom
+- Date de la commande
+"""
